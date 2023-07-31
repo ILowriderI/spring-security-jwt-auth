97273f12de20bb72c904ec2b15907d1cd820036a
ILowriderI <126571170+ILowriderI@users.noreply.github.com>
2023-07-31 17:24:51 +0300

    ref

--- src/main/java/com/example/securityjwtauth/config/ApplicationConfig.java
+++ src/main/java/com/example/securityjwtauth/config/ApplicationConfig.java
@@ -17,30 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class ApplicationConfig {
     private final UserRepository userRepository;
-
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        return username -> userRepository.findByEmail(username)
-//                .orElseThrow(()-> new  UsernameNotFoundException("user not found"));
-//    }
-//
-//    @Bean
-//    public AuthenticationProvider authenticationProvider(){
-//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setUserDetailsService(userDetailsService());
-//        authenticationProvider.setPasswordEncoder(passwordEncoder());
-//        return  authenticationProvider;
-//    }
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-//        return  configuration.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return  new BCryptPasswordEncoder();
-//    }
-
+    
 
     @Bean
     public UserDetailsService userDetailsService() {
